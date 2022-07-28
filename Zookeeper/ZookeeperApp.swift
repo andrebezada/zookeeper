5b2f52feec01dd2f29dd28db684fb8489570e570
Andre Bezada <a.bezada.maza@gmail.com>
2022-07-27 21:13:26 -0500

    Initial Commit

---
+++ Zookeeper/ZookeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZookeeperApp.swift
+//  Zookeeper
+//
+//  Created by Andre Bezada on 27/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ZookeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
